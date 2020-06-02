0a073e76c2239a25e0aaa8299a0d201fb8696806
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-06-02 16:56:55 -0600

    2020-06-02 16:56, latest updates, using Python to commit

--- backend/python/reconcileArrays/reconcileArrays.py
+++ backend/python/reconcileArrays/reconcileArrays.py
@@ -8,121 +8,127 @@ import sys
 
 import gspread
 
-def reconcileArraysFunction(runningOnProductionServerBoolean):
+def reconcileArraysFunction(runningOnProductionServerBoolean, privacy):
 
 	pathToThisPythonFile = Path(__file__).resolve()
 
-	if runningOnProductionServerBoolean:
-		p('********************Running on production server****************')
-		from ..myPythonLibrary import _myPyFunc
-		from ..googleSheets.myGoogleSheetsLibrary import _myGoogleSheetsFunc
-		from ..googleSheets.myGoogleSheetsLibrary import _myGspreadFunc
+	if privacy == 'Private':
 
-		loadedEncryptionKey = os.environ.get('savedEncryptionKeyStr', None)
-		
+		pass
+
 	else:
-		p('********************Not running on production server****************')
 
-		sys.path.append(str(pathToThisPythonFile.parents[1]))
-		from myPythonLibrary import _myPyFunc
-		from googleSheets.myGoogleSheetsLibrary import _myGoogleSheetsFunc
-		from googleSheets.myGoogleSheetsLibrary import _myGspreadFunc
+		if runningOnProductionServerBoolean:
+			p('********************Running on production server****************')
+			from ..myPythonLibrary import _myPyFunc
+			from ..googleSheets.myGoogleSheetsLibrary import _myGoogleSheetsFunc
+			from ..googleSheets.myGoogleSheetsLibrary import _myGspreadFunc
 
-		pathToRepos = _myPyFunc.getPathUpFolderTree(pathToThisPythonFile, 'repos')
-		loadedEncryptionKey = _myPyFunc.openSavedKey(Path(pathToRepos, 'privateData', 'python', 'encryption', 'savedEncryptionKey.key'))
-	
+			loadedEncryptionKey = os.environ.get('savedEncryptionKeyStr', None)
+			
+		else:
+			p('********************Not running on production server****************')
 
-	strToReturn = 'hellowasdf'
-	strToReturn = os.environ.get('urlOfKingGorillaGoogleSheetPublicStr')
+			sys.path.append(str(pathToThisPythonFile.parents[1]))
+			from myPythonLibrary import _myPyFunc
+			from googleSheets.myGoogleSheetsLibrary import _myGoogleSheetsFunc
+			from googleSheets.myGoogleSheetsLibrary import _myGspreadFunc
 
-	if not strToReturn:
-
-		pathToConfigDataJSON = Path(pathToRepos, 'privateData', 'herokuGorilla', 'configData.json')
-
-		jsonFileObj = open(pathToConfigDataJSON)
-		strToReturn = json.load(jsonFileObj)['urlOfKingGorillaGoogleSheetPublicStr']
-	
-	strToReturn = strToReturn[:-1] + '871892682'
-
-
-	pathToAPIKey = Path(pathToThisPythonFile.parents[2], 'configData', 'encryptedAPIKey.json')
-	pathOfDecryptedFile = Path(pathToAPIKey.parents[0], 'decryptedAPIKey.json')
-	_myPyFunc.decryptFile(pathToAPIKey, loadedEncryptionKey, pathToSaveDecryptedFile=pathOfDecryptedFile)
+			pathToRepos = _myPyFunc.getPathUpFolderTree(pathToThisPythonFile, 'repos')
+			loadedEncryptionKey = _myPyFunc.openSavedKey(Path(pathToRepos, 'privateData', 'python', 'encryption', 'savedEncryptionKey.key'))
 		
-	gspObj = gspread.service_account(filename=pathOfDecryptedFile)
+
+		strToReturn = 'hellowasdf'
+		strToReturn = os.environ.get('urlOfKingGorillaGoogleSheetPublicStr')
+
+		if not strToReturn:
+
+			pathToConfigDataJSON = Path(pathToRepos, 'privateData', 'herokuGorilla', 'configData.json')
+
+			jsonFileObj = open(pathToConfigDataJSON)
+			strToReturn = json.load(jsonFileObj)['urlOfKingGorillaGoogleSheetPublicStr']
 		
-	gspSpreadsheet = gspObj.open('King Gorilla - Public')
-	gspFirstTableSheet = gspSpreadsheet.worksheet('firstTable')
-	gspSecondTableSheet = gspSpreadsheet.worksheet('secondTable')
-	gspComparisonTableSheet = gspSpreadsheet.worksheet('comparisonTable')
-	gspEndingSecondTableSheet = gspSpreadsheet.worksheet('endingSecondTable')
-
-	firstArray = gspFirstTableSheet.get_all_values()
-	secondArray = gspSecondTableSheet.get_all_values()
-	firstArrayFirstRow = firstArray.pop(0)
-	secondArrayFirstRow = secondArray.pop(0)
+		strToReturn = strToReturn[:-1] + '871892682'
 
 
-	matchingColumnTitle = ''
+		pathToAPIKey = Path(pathToThisPythonFile.parents[2], 'configData', 'encryptedAPIKey.json')
+		pathOfDecryptedFile = Path(pathToAPIKey.parents[0], 'decryptedAPIKey.json')
+		_myPyFunc.decryptFile(pathToAPIKey, loadedEncryptionKey, pathToSaveDecryptedFile=pathOfDecryptedFile)
+			
+		gspObj = gspread.service_account(filename=pathOfDecryptedFile)
+			
+		gspSpreadsheet = gspObj.open('King Gorilla - Public')
+		gspFirstTableSheet = gspSpreadsheet.worksheet('firstTable')
+		gspSecondTableSheet = gspSpreadsheet.worksheet('secondTable')
+		gspComparisonTableSheet = gspSpreadsheet.worksheet('comparisonTable')
+		gspEndingSecondTableSheet = gspSpreadsheet.worksheet('endingSecondTable')
 
-	for indexOfColumnIndexFirstArray, columnTitleFirstArray in enumerate(firstArrayFirstRow):
-		for indexOfColumnIndexSecondArray, columnTitleSecondArray in enumerate(secondArrayFirstRow):
-			if columnTitleFirstArray == columnTitleSecondArray:
-				firstArrayColumnIndexToCompare = indexOfColumnIndexFirstArray
-				secondArrayColumnIndexToCompare = indexOfColumnIndexSecondArray
-
-	comparisonArray = [['firstTable'] + [''] * (len(firstArray[0])) + ['secondTable'] + [''] * (len(secondArray[0]) - 1)]
-	comparisonArray.append(firstArrayFirstRow + [''] + secondArrayFirstRow)
-	# p(comparisonArray)
+		firstArray = gspFirstTableSheet.get_all_values()
+		secondArray = gspSecondTableSheet.get_all_values()
+		firstArrayFirstRow = firstArray.pop(0)
+		secondArrayFirstRow = secondArray.pop(0)
 
 
-	while firstArray:
+		matchingColumnTitle = ''
 
-		firstArrayCurrentRow = firstArray.pop(0)
-		# p(firstArrayCurrentRow)
-		rowToAppend = firstArrayCurrentRow + ['']
+		for indexOfColumnIndexFirstArray, columnTitleFirstArray in enumerate(firstArrayFirstRow):
+			for indexOfColumnIndexSecondArray, columnTitleSecondArray in enumerate(secondArrayFirstRow):
+				if columnTitleFirstArray == columnTitleSecondArray:
+					firstArrayColumnIndexToCompare = indexOfColumnIndexFirstArray
+					secondArrayColumnIndexToCompare = indexOfColumnIndexSecondArray
 
-		for secondArrayRowIndexCount, secondArrayCurrentRow in enumerate(secondArray):
-
-			# p(secondArrayCurrentRow)
-
-			if firstArrayCurrentRow[firstArrayColumnIndexToCompare] == secondArrayCurrentRow[secondArrayColumnIndexToCompare]:
-
-				secondArrayRowToAppend = secondArray.pop(secondArrayRowIndexCount)
-				rowToAppend = rowToAppend + secondArrayRowToAppend
-
-		comparisonArray.append(rowToAppend)
+		comparisonArray = [['firstTable'] + [''] * (len(firstArray[0])) + ['secondTable'] + [''] * (len(secondArray[0]) - 1)]
+		comparisonArray.append(firstArrayFirstRow + [''] + secondArrayFirstRow)
 		# p(comparisonArray)
 
 
-	clearAndResizeParameters = [{
-		'sheetObj': gspComparisonTableSheet,
-		'resizeRows': 3,
-		'startingRowIndexToClear': 0
-	},
-	{
-		'sheetObj': gspEndingSecondTableSheet,
-		'resizeRows': 2,
-		'startingRowIndexToClear': 0
-	}]
+		while firstArray:
 
+			firstArrayCurrentRow = firstArray.pop(0)
+			# p(firstArrayCurrentRow)
+			rowToAppend = firstArrayCurrentRow + ['']
+
+			for secondArrayRowIndexCount, secondArrayCurrentRow in enumerate(secondArray):
+
+				# p(secondArrayCurrentRow)
+
+				if firstArrayCurrentRow[firstArrayColumnIndexToCompare] == secondArrayCurrentRow[secondArrayColumnIndexToCompare]:
+
+					secondArrayRowToAppend = secondArray.pop(secondArrayRowIndexCount)
+					rowToAppend = rowToAppend + secondArrayRowToAppend
+
+			comparisonArray.append(rowToAppend)
+			# p(comparisonArray)
+
+
+		clearAndResizeParameters = [{
+			'sheetObj': gspComparisonTableSheet,
+			'resizeRows': 3,
+			'startingRowIndexToClear': 0
+		},
+		{
+			'sheetObj': gspEndingSecondTableSheet,
+			'resizeRows': 2,
+			'startingRowIndexToClear': 0
+		}]
+
+
+		
+		_myGspreadFunc.clearAndResizeSheets(clearAndResizeParameters)
+
+
+		_myGspreadFunc.updateCells(gspComparisonTableSheet, comparisonArray)
+
+
+		secondArray.insert(0, secondArrayFirstRow)
+		_myGspreadFunc.updateCells(gspEndingSecondTableSheet, secondArray)
 
 	
-	_myGspreadFunc.clearAndResizeSheets(clearAndResizeParameters)
+		with open(pathOfDecryptedFile, "w") as fileObj:
+			fileObj.write('')
 
 
-	_myGspreadFunc.updateCells(gspComparisonTableSheet, comparisonArray)
-
-
-	secondArray.insert(0, secondArrayFirstRow)
-	_myGspreadFunc.updateCells(gspEndingSecondTableSheet, secondArray)
-
- 
-	with open(pathOfDecryptedFile, "w") as fileObj:
-		fileObj.write('')
-
-
-	return strToReturn
+		return strToReturn
 
 
 
