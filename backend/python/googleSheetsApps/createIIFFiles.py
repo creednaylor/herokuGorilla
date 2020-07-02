593ef4cfe3225ed0a0a53de49fc3d0acfc562987
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-07-01 20:15:52 -0600

    2020-07-01 20:15, latest updates, using Python to commit

--- backend/python/googleSheetsApps/createIIFFiles.py
+++ backend/python/googleSheetsApps/createIIFFiles.py
@@ -21,17 +21,20 @@ else:
 
 def createIIFFilesFunction(oAuthMode, googleSheetTitle, loadSavedCredentials=True, listingService='Airbnb'):
 
-	# pathToRepos = _myPyFunc.getPathUpFolderTree(pathToThisPythonFile, 'repos')
 	pathToThisProjectRoot = pathToThisPythonFile.parents[3]
 	gspObj = _myGspreadFunc.authorizeGspread(oAuthMode, pathToThisProjectRoot, loadSavedCredentials)
 
 	gspSpreadsheet = gspObj.open(googleSheetTitle)
 	gspInput = gspSpreadsheet.worksheet('Input')
 	gspOutput = gspSpreadsheet.worksheet('Output')
-	gspMap = gspSpreadsheet.worksheet('Map')
+	gspHostFeesMap = gspSpreadsheet.worksheet('Map - Host Fees')
+	gspRentRevenueMap = gspSpreadsheet.worksheet('Map - Listing')
+	gspPayoutMap = gspSpreadsheet.worksheet('Map - Details')
 	inputArray = gspInput.get_all_values()
 	outputArray = gspOutput.get_all_values()
-	mapArray = gspMap.get_all_values()
+	rentRevenueMapArray = gspRentRevenueMap.get_all_values()
+	hostFeesMapArray = gspHostFeesMap.get_all_values()
+	payoutMapArray = gspPayoutMap.get_all_values()
 
 	del outputArray[3:]
 	outputArray.append([''])
@@ -45,7 +48,7 @@ def createIIFFilesFunction(oAuthMode, googleSheetTitle, loadSavedCredentials=Tru
 
 			for dataToWriteColumnIndex in range(0, len(currentDataToWriteRow)):
 
-				if dataToWriteRowIndex == len(dataToWrite) - 1 and dataToWriteColumnIndex == 0:
+				if dataToWriteRowIndex != 0 and dataToWriteColumnIndex == 0:
 					rowToWrite.append('SPL')
 				elif dataToWriteColumnIndex == 3:
 					rowToWrite.append(payoutDate)
@@ -74,17 +77,35 @@ def createIIFFilesFunction(oAuthMode, googleSheetTitle, loadSavedCredentials=Tru
 					dataToWrite = writeData(dataToWrite, payoutDate)
 
 				payoutDate = inputRow[0]
-				transactionAccount = '"Checking"'
+
+				for payoutMapArrayRow in payoutMapArray[1:]:
+					if payoutMapArrayRow[0] == inputRow[7]:
+						transactionAccount = '"' + payoutMapArrayRow[1] + '"'
 			
 			else:
 
 				if inputRow[6] == '':
 					transactionAccount = 'Need to input manually'
 
-				for mapArrayRow in mapArray[1:]:
-					if mapArrayRow[0] == inputRow[6]:
-						transactionAccount = '"' + mapArrayRow[1] + '"'
+				for rentRevenueMapArrayRow in rentRevenueMapArray[1:]:
+					if rentRevenueMapArrayRow[0] == inputRow[6]:
+						transactionAccount = '"' + rentRevenueMapArrayRow[1] + '"'
 
+				for hostFeesMapArrayRow in hostFeesMapArray[1:]:
+					if hostFeesMapArrayRow[0] == inputRow[6]:
+						hostFeesAccount = '"' + hostFeesMapArrayRow[1] + '"'
+	
+			confirmationCode = 'Confirmation Code: ' + inputRow[2] + '; Airbnb Transaction Type: ' + inputRow[1] + '; Guest: ' + inputRow[5] + '; Nights: ' + inputRow[4]	
+
+			if inputRow[2] == '':
+				confirmationCode = 'Airbnb Transaction Type: ' + inputRow[1] + '; ' + inputRow[7]
+
+
+			if inputRow[12] == '':
+				hostFeeAmount = float('0'.replace(',', ''))
+			else:
+				hostFeeAmount = float(inputRow[12].replace(',', ''))
+				dataToWrite.append(['TRNS', '', 'General Journal', '', hostFeesAccount, 'AirBNB', '', hostFeeAmount, '', confirmationCode])
 
 			if inputRow[10] == '':
 				inputRow[10] = '0'
@@ -94,15 +115,13 @@ def createIIFFilesFunction(oAuthMode, googleSheetTitle, loadSavedCredentials=Tru
 				inputRow[11] = '0'
 			paidOutAmount = float(inputRow[11].replace(',', ''))
 
-			confirmationCode = 'Confirmation Code: ' + inputRow[2] + '; Airbnb Transaction Type: ' + inputRow[1] + '; Guest: ' + inputRow[5] + '; Nights: ' + inputRow[4]	
-
-			if inputRow[2] == '':
-				confirmationCode = 'Airbnb Transaction Type: ' + inputRow[1] + '; ' + inputRow[7]
-
-			dataToWrite.append(['TRNS', '', 'General Journal', '', transactionAccount, '', '', paidOutAmount - transactionAmount, '', confirmationCode])
+			dataToWrite.append(['TRNS', '', 'General Journal', '', transactionAccount, 'AirBNB', '', paidOutAmount - transactionAmount - hostFeeAmount, '', confirmationCode])
 			
 			if inputRowIndex == len(inputArray) - 1:
 				writeData(dataToWrite, payoutDate)
+
+
+
 
 		if listingService == 'VRBO':
 
