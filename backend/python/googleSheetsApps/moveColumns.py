301a144b06929d6bcc8f5a18b2fdb03f8a0ab5f6
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-08-04 18:17:24 -0600

    2020-08-04 18:17, latest updates, using Python to commit

---
+++ backend/python/googleSheetsApps/moveColumns.py
@@ -0,0 +1,26 @@
+{
+  "requests": [
+    {
+      "moveDimension": {
+        "source": {
+          "sheetId": sheetId,
+          "dimension": "COLUMNS",
+          "startIndex": 0,
+          "endIndex": 1
+        },
+        "destinationIndex": 3
+      }
+    },
+    {
+      "moveDimension": {
+        "source": {
+          "sheetId": sheetId,
+          "dimension": "ROWS",
+          "startIndex": 4,
+          "endIndex": 10
+        },
+        "destinationIndex": 19
+      }
+    },
+  ],
+}
