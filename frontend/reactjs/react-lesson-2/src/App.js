718ea4cc2bc1599bd45df4d82a7e1bb2fca4360a
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-09-09 09:39:02 -0600

    2020-09-09 09:38, latest updates, using Python to commit

--- frontend/reactjs/react-lesson-2/src/App.js
+++ frontend/reactjs/react-lesson-2/src/App.js
@@ -1,16 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 import Header from './Header'
 
 
+console.log("outside the function");
+
+
 function App() {
   
-  let nameOfPerson = window.prompt("Please enter your name", "Harry Potter");
+  const [nameOfPerson, setNameOfPerson] = useState('Harry Potter');
+
+  const onNameChange = (event) => {
+    let newNameOfPerson = event.target.value;
+    setNameOfPerson(newNameOfPerson);
+  }
+
+  useEffect(() => {
+    let newNameOfPerson = window.prompt('What is your name?', nameOfPerson);
+    setNameOfPerson(newNameOfPerson);
+  }, []);
 
   return (
     <div className="App">
       <Header userName={nameOfPerson}/>
+      <input type="text" onChange={onNameChange}/>
     </div>
   );
 }
--- frontend/reactjs/react-lesson-2/src/Header.js
+++ frontend/reactjs/react-lesson-2/src/Header.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import Anchorlink from './Anchorlink';
 
 export default function Header(props) {
     return (
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello {props.userName}!
         </p>
