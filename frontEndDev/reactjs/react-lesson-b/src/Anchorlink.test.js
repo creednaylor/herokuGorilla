4cc2fdb6d5ef599b6ca17e188e8a244784bb09a3
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-09-28 10:58:19 -0600

    2020-09-28 10:58, latest updates, using Python to commit

--- frontEndDev/reactjs/react-lesson-b/src/Anchorlink.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Anchorlink from './Anchorlink';
-
-
-test('test of Anchorlink', () => {
-    const { getByText } = render(<Anchorlink />);
-});
--- frontEndDev/reactjs/react-lesson-b/src/Header.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Header from './Header';
-
-
-test('test of Header', () => {
-    const { getByText } = render(<Header />);
-});
