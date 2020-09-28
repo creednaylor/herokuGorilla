7ff33a8674bfe9be039b03c1fb50f273dffbb480
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-09-28 09:35:39 -0600

    2020-09-28 09:35, latest updates, using Python to commit

---
+++ frontEndDev/reactjs/react-lesson-d/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [consoleMessage, setConsoleMessage] = useState('');
+
+  const submitButtonHandler = () => {
+
+    if (username == 'username' && password == 'a') {
+      setConsoleMessage(`Login successful. Username: ${username}. Password: ${password}.`);
+    }
+    else {
+      setConsoleMessage(`Login failed.`)
+    }
+
+  }
+
+  const usernameHandler = (event) => {
+    setUsername(event.target.value);
+  }
+
+  const passwordHandler = (event) => {
+    setPassword(event.target.value);
+  }
+
+  return (
+    <div className="App">
+      <form>
+        Username: <input type="text" defaultValue="" onChange={usernameHandler}/>
+        <br></br>
+        Password: <input type="password" defaultValue="" onChange={passwordHandler} pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required/>
+        <br></br>
+        <input type="submit" value="Submit" onClick={submitButtonHandler} />
+      </form>
+      {consoleMessage}
+    </div>
+  );
+}
+
+export default App;
