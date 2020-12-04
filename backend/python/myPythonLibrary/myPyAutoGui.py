60a8d767af68181321a94b447a0b6cd1a6f34146
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-12-03 19:33:31 -0700

    2020-12-03 19:33, added/committed/pushed using custom Python script

--- backend/python/myPythonLibrary/myPyAutoGui.py
+++ backend/python/myPythonLibrary/myPyAutoGui.py
@@ -3,7 +3,7 @@ from pprint import pprint as p
 from pathlib import Path
 
 
-def typeCharactersOnRemoteDesktop(charactersToType, priorPyAutoGuiPause, pause=None):
+def typeCharactersOnRemoteDesktop(charactersToType, pause=None):
 
     charactersNeedingShift = (list(range(123, 127)) + list(range(94, 96)) + list(range(62, 91)) + [60, 58] + list(range(40, 44)) + list(range(33, 39)))
     
@@ -12,10 +12,9 @@ def typeCharactersOnRemoteDesktop(charactersToType, priorPyAutoGuiPause, pause=N
 
         if ord(characterToType) in charactersNeedingShift:
 
-            if pause == None:
-                g.PAUSE = .0001
-            else:
-                g.PAUSE = pause
+            priorPyAutoGuiPause = g.PAUSE
+            if pause == None: pause = .0001
+            g.PAUSE = pause
 
             # g.hotkey('shift', characterToType)
             g.keyDown('shift')
@@ -43,10 +42,19 @@ def clickWhenLocalPNGAppears(pngFileStem, pngFileDirectory, confidence=.9):
     pngFilePath = str(Path(pngFileDirectory, pngFileStem + '.png'))
     clickImageWhenAppears(pngFilePath, confidence=confidence)
 
+def doubleClickWhenLocalPNGAppears(pngFileStem, pngFileDirectory, confidence=.9):
+
+    pngFilePath = str(Path(pngFileDirectory, pngFileStem + '.png'))
+
+    coordinates = getCoordinatesWhenLocalPNGAppears(pngFileStem, pngFileDirectory)
+    
+    g.click(x=coordinates.x, y=coordinates.y, clicks=2)
+
+
 def getCoordinatesWhenLocalPNGAppears(pngFileStem, pngFileDirectory, confidence=.9, center=True):
 
     pngFilePath = str(Path(pngFileDirectory, pngFileStem + '.png'))
-    getCoordinatesWhenImageAppears(pngFilePath, confidence=.9, center=True)
+    return getCoordinatesWhenImageAppears(pngFilePath, confidence=.9, center=True)
 
 def getCoordinatesWhenImageAppears(imageFileName, confidence=.9, center=True):
 
@@ -62,7 +70,16 @@ def getCoordinatesWhenImageAppears(imageFileName, confidence=.9, center=True):
 
     return coordinatesToReturn
 
-def getCoordinatesIfLocalPNGIsShowing(pngFileStem, pngFileDirectory, confidence=.9):
+
+def locateOnScreenLocal(pngFileStem, pngFileDirectory):
+
+    pngFilePath = str(Path(pngFileDirectory, pngFileStem + '.png'))
+
+    return g.locateOnScreen(pngFilePath, confidence=.9)
+
+
+
+def getCoordinatesIfLocalPNGIsShowing(pngFileStem, pngFileDirectory, confidence=.96):
 
     pngFilePath = str(Path(pngFileDirectory, pngFileStem + '.png'))
 
@@ -74,6 +91,13 @@ def waitUntilLocalPNGDisappears(pngFileStem, pngFileDirectory):
     pngFilePath = str(Path(pngFileDirectory, pngFileStem + '.png'))
 
     waitUntilImageDisappears(pngFilePath)
+
+
+def waitUntilLocalPNGAppears(pngFileStem, pngFileDirectory):
+
+    pngFilePath = str(Path(pngFileDirectory, pngFileStem + '.png'))
+
+    waitUntilImageAppears(pngFilePath)
 
 
 def waitUntilImageDisappears(imageFileName, confidence=.9, center=True):
@@ -89,3 +113,63 @@ def waitUntilImageDisappears(imageFileName, confidence=.9, center=True):
             coordinatesToReturn = g.locateOnScreen(imageFileName, confidence=confidence)
 
     return coordinatesToReturn
+
+def waitUntilImageAppears(imageFileName, confidence=.9, center=True):
+
+    coordinatesToReturn = g.locateOnScreen(imageFileName, confidence=confidence)
+
+    while not coordinatesToReturn:
+        p(f'Waiting for {imageFileName} to appear.')
+
+        if center:
+            coordinatesToReturn = g.locateCenterOnScreen(imageFileName, confidence=confidence)
+        else:
+            coordinatesToReturn = g.locateOnScreen(imageFileName, confidence=confidence)
+
+    return coordinatesToReturn
+
+def typeStr(strToType):
+
+    for characterToType in strToType:
+        if characterToType == '_':
+            g.keyDown('shift')
+            g.press(characterToType)
+            g.keyUp('shift')
+        else:
+            g.press(characterToType)
+
+def clickLocalPNGWhenAppearsAndWaitUntilLocaPNGDisappears(imageFileNameToClick, imageFileNameToWaitUntilDisappears, parentDir):
+
+    clickWhenLocalPNGAppears(imageFileNameToClick, parentDir)
+    waitUntilLocalPNGDisappears(imageFileNameToWaitUntilDisappears, parentDir)
+
+
+def typeAndWriteOnRemoteDesktop(charactersToType, pause=None):
+
+    charactersNeedingShift = (list(range(123, 127)) + list(range(94, 96)) + list(range(62, 91)) + [60, 58] + list(range(40, 44)) + list(range(33, 39)))
+    
+    stringToWrite = ''
+
+    for characterToType in charactersToType:
+
+        if ord(characterToType) in charactersNeedingShift:
+
+            if stringToWrite: 
+                g.write(stringToWrite)
+                stringToWrite = ''
+
+            priorPyAutoGuiPause = g.PAUSE
+
+            if pause == None: pause = .0001
+            g.PAUSE = pause
+
+            # g.hotkey('shift', characterToType)
+            g.keyDown('shift')
+            g.press(characterToType)
+            g.keyUp('shift')
+            g.PAUSE = priorPyAutoGuiPause
+
+        else:
+            stringToWrite = stringToWrite + characterToType
+    
+    if stringToWrite: g.write(stringToWrite)
